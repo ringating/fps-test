4a1f3f9c0b0382359e13a0e78b96f9985b4e52f7
Chris <34548930+ringating@users.noreply.github.com>
2019-11-05 01:43:24 -0800

    added comment "this doesnt work at all..."

--- scripts/toggle_fps_cap.gd
+++ scripts/toggle_fps_cap.gd
@@ -1,9 +1,13 @@
 extends Spatial
 
+# this doesnt work at all...
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	ProjectSettings.set("debug/settings/fps/force_fps", 10)
+	print("initial force_fps: %d" % ProjectSettings.get_setting("debug/settings/fps/force_fps"))
+	ProjectSettings.set_setting("debug/settings/fps/force_fps", 10)
 	ProjectSettings.save()
+	print("force_fps: %d" % ProjectSettings.get_setting("debug/settings/fps/force_fps"))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
